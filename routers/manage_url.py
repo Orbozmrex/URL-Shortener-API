32b5aa72d06dc98bc57033873c312b633a7ab42e
Your a <a@example.com>
2026-05-21 14:28:29 +0300

    URL not found exception

--- routers/manage_url.py
+++ routers/manage_url.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
-from utils.db import get_url_stats
+from fastapi import APIRouter, HTTPException
+from utils.db import get_url_stats, get_url
 
 router = APIRouter(prefix="/manage")
 
 @router.get("/stats/{token}")
 async def get_stats(token: str):
+    if not await get_url(token):
+        raise HTTPException(status_code=404, detail="URL not found")
     return {'token': token, 'stats': await get_url_stats(token)}
