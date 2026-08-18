c90a00dcc9298e4536b94ab57b65375b46ccad0e
Your a <a@example.com>
2026-08-18 15:41:34 +0300

    removed dropall from init_db function

--- utils/database.py
+++ utils/database.py
@@ -80,5 +80,4 @@ async def add_visit(token):
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
