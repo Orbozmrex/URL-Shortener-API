5e767f8622e5f57d9aa6d78d462d47faeeac1002
Your a <a@example.com>
2026-08-18 20:47:47 +0300

    Url schemas

---
+++ schemas/url.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+
+class Url(BaseModel):
+    url: str
+    token: str
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class Stats(Url):
+    visits: int
