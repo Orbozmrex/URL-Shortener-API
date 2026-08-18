9ab8605bf8cf989a11bd534f9dcf0fa26f98f33d
Your a <a@example.com>
2026-08-18 20:45:58 +0300

    Added: manage/info/{token} endpoint

--- routers/manage_url.py
+++ routers/manage_url.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter, HTTPException
-from utils.database import get_url
+from fastapi import APIRouter, HTTPException, Depends
+from utils.jwt_handlers import get_current_user
+from utils.database import get_url, get_url_info
+from typing import Annotated
+from schemas.user import UserSchema
 
-router = APIRouter(prefix="/manage")
+router = APIRouter(prefix="/manage", tags=["manage url"])
 
-# @router.get("/stats/{token}")
-# async def get_stats(token: str):
-#     if not await get_url(token):
-#         raise HTTPException(status_code=404, detail="URL not found")
-#     return {'token': token, 'stats': await get_url_stats(token)}
+@router.get("/info/{token}")
+async def get_stats(token: str, current_user: Annotated[UserSchema, Depends(get_current_user)]) -> dict:
+    url = await get_url(token)
+    if not url:
+        raise HTTPException(status_code=404, detail="URL not found")
+    if not url.owner_id == current_user.id:
+        raise HTTPException(status_code=403, detail="Forbidden resource")
+    stats = await get_url_info(token)
+    return {'stats': stats}
